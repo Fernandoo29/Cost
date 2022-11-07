63a7ed9f61ee3b90b45dbf27980b9fda1e24c212
Fernandoo29 <86057233+Fernandoo29@users.noreply.github.com>
2022-11-07 18:34:29 -0300

    Aula #19 - Implementação do Header e do Footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'; 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
 
 import Home from './Components/pages/Home';
+import Project from './Components/pages/Projects';
 import Company from './Components/pages/Company';
 import Contact from './Components/pages/Contact';
 import NewProject from './Components/pages/NewProject';
 
 import Container from './Components/layout/Container';
+import Navbar from './Components/layout/Navbar';
+import Footer from './Components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contacto</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route exact element={<Home />} path='/'></Route>
+          <Route path='/project' element={<Project />}></Route>
           <Route path='/company' element={<Company />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
           <Route path='/newproject' element={<NewProject />}></Route>
         </Routes>
       </Container>
-      <footer>Footer</footer>
+      <Footer />
     </Router>
   );
 }
