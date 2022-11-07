93581af8ab433beee83afc736311ec5fd0ec4469
Fernandoo29 <86057233+Fernandoo29@users.noreply.github.com>
2022-11-06 21:20:05 -0300

    Aula #18 - Implementação do sistema de rotas, das páginas e implementação parcial dos componentes de layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'; 
+
+import Home from './Components/pages/Home';
+import Company from './Components/pages/Company';
+import Contact from './Components/pages/Contact';
+import NewProject from './Components/pages/NewProject';
+
+import Container from './Components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <p>Cost</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contacto</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact element={<Home />} path='/'></Route>
+          <Route path='/company' element={<Company />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/newproject' element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
