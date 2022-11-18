771aa0e8630db4778e2305865e6fc48e7d6ed56c
Fernandoo29 <86057233+Fernandoo29@users.noreply.github.com>
2022-11-18 12:48:59 -0300

    Aula #26 - Implementação parcial do dashboard dos projetos criados

--- src/Components/pages/Projects.js
+++ src/Components/pages/Projects.js
@@ -1,6 +1,10 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
-import Message from "../layout/Message"
+import styles from './Projects.module.css';
+
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 function Project(){
     const location = useLocation();
@@ -11,10 +15,17 @@ function Project(){
     }
 
     return (
-        <div>
-            <h1>Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
+
             {message && <Message msg={message} type="success" />}
-            
+
+            <Container customClass="start">
+                <p>Projetos....</p>
+            </Container>
         </div>
     )
 }
